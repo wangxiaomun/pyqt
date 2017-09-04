05b2c301cc6b801b07384fc4dbbfdef2aece0199
wangxiaomun <wangxiaomun@gmail.com>
2017-09-04 15:39:12 +0000

    add wifi page

--- WiFiController.py
+++ WiFiController.py
@@ -2,16 +2,20 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel,QPushButton
 from PyQt5.QtWidgets import QVBoxLayout,QHBoxLayout,QProxyStyle
 from PyQt5.QtGui import QIcon
+from wifiUI import wifiUI
 
 class WiFiController(QPushButton):
     def __init__(self):
         super(WiFiController, self).__init__()
         self.setText("Wi-Fi")
         self.clicked.connect(lambda: self.clickHandler())
-        self.stack = QWidget()
+        self.wifiUI = wifiUI()
+
+
+    def setQWidget(self,widget):
+        self.widget = widget
+        self.wifiUI.setQWidget(widget)
 
     def clickHandler(self):
-        hbox = QHBoxLayout()
-        hbox.addWidget(QPushButton("nine"))
-        hbox.addWidget(QPushButton("ten"))
-        self.stack.setLayout(hbox)
+        self.widget.hide()
+        self.wifiUI.show()
--- wifiUI.py
+++ wifiUI.py
@@ -1,43 +1,38 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel,QPushButton
 from PyQt5.QtWidgets import QVBoxLayout,QHBoxLayout,QProxyStyle
-from WiFiController import WiFiController
 
-class mainUI(QWidget):
+class wifiUI(QWidget):
     def __init__(self):
-        super(mainUI, self).__init__()
+        super(wifiUI, self).__init__()
         self.leftlist = QVBoxLayout()
         self.leftlist.addSpacing(20)
 
         self.rigthlist = QVBoxLayout()
         self.rigthlist.addSpacing(20)
 
-        self.wificontroller = WiFiController()
-        self.wificontroller.setQWidget(self)
-
-        self.leftlist.addWidget(QPushButton("one"))
-        self.leftlist.addWidget(QPushButton("three"))
-        self.leftlist.addWidget(QPushButton("five"))
-        self.leftlist.addWidget(self.wificontroller)
-
+        self.backbutton = QPushButton("Back")
+        self.leftlist.addWidget(self.backbutton)
         self.rigthlist.addWidget(QPushButton("two"))
-        self.rigthlist.addWidget(QPushButton("four"))
-        self.rigthlist.addWidget(QPushButton("six"))
-        self.rigthlist.addWidget(QPushButton("eight"))
-
+        self.backbutton.clicked.connect(lambda: self.clickHandler())
         hbox = QHBoxLayout()
         hbox.addLayout(self.leftlist)
         hbox.addLayout(self.rigthlist)
 
-
         self.setLayout(hbox)
         self.setGeometry(300, 300, 800, 480)
         self.setCSS()
-        self.show()
+
+    def setQWidget(self,widget):
+        self.widget = widget
+
+    def clickHandler(self):
+        self.hide()
+        self.widget.show()
 
     def setCSS(self):
         self.setStyleSheet("""
-            .QPushButton,.WiFiController {
+            .QPushButton{
                 background:  rgb(66, 184, 221); /* #4CAF50;  Green */
                 color: white;
                 padding: 20px 40px;
@@ -50,15 +45,11 @@ class mainUI(QWidget):
                 border: 4px solid black;
                 background: grey
                 }
-            .WiFiController:pressed {
-                border: 4px solid black;
-                background: grey
-                }
             """)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     myStyle = QProxyStyle('Fusion')
     app.setStyle(myStyle)
-    ex = mainUI()
+    ex = wifiUI()
     sys.exit(app.exec_())
